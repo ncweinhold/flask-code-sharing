98c9ede29e8b36458a02c82dfb4083de8aa34fc4
Nick Weinhold <n.c.weinhold@googlemail.com>
2012-12-11 12:33:55 +0000

    Adding templates and frontend features.
    This commit adds the ability to register, log in and log out. Also
    unit tests have been written to test the behaviour up to this point.
    
    Templates are still very basic, and need more work.

--- pasteapp/views/frontend.py
+++ pasteapp/views/frontend.py
@@ -1,7 +1,10 @@
 from flask import (
-    Blueprint, render_template, request,
-    flash, redirect, url_for, abort
+    Blueprint, render_template, request, flash, redirect, url_for, abort,
+    session
 )
+
+from pasteapp.forms import RegistrationForm, LoginForm
+from pasteapp.database import User, db_session
 
 frontend = Blueprint('frontend', __name__)
 
@@ -14,6 +17,59 @@ def index():
     """
     return render_template('index.html')
 
+@frontend.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    err = None
+    if form.validate_on_submit():
+        if username_taken(form.username.data):
+            return render_template('register.html',
+                                   form=form,
+                                   err='Username has already been taken.')
+        if email_taken(form.email.data):
+            return render_template('register.html',
+                                   form=form,
+                                   err='Email address has already been used.')
+        user = User(form.username.data,
+                    form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        db_session.commit()
+        flash('You have successfully registered and can now log in.')
+        return redirect(url_for('frontend.login'))
+    return render_template('register.html', form=form, err=err)
+
+@frontend.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    err = None
+    if form.validate_on_submit():
+        user = User.query.filter(User.username == form.username.data).first()
+        if not user or not user.check_bcrypt_hash(form.password.data):
+            err = "Invalid username or password"
+            return render_template('login.html', form=form, err=err)
+        else:
+            session['user_id'] = user.id
+            flash('You have logged in successfully.')
+            return redirect(url_for('frontend.dashboard'))
+    return render_template('login.html', form=form, err=err)
+
+@frontend.route('/logout')
+def logout():
+    if 'user_id' not in session:
+        return redirect(url_for('frontend.index'))
+    flash('Logged out successfully')
+    session.pop('user_id', None)
+    return redirect(url_for('frontend.index'))
+
+@frontend.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        flash('You must log in first')
+        return redirect(url_for('frontend.login'))
+    else:
+        return render_template('dashboard.html')
+
 @frontend.route('/about')
 def about():
     return render_template('about.html')
@@ -21,3 +77,9 @@ def about():
 @frontend.route('/paste/<int:paste_id>')
 def view_paste(paste_id):
     return render_template('view_paste.html')
+
+def username_taken(username):
+    return User.query.filter(User.username == username).first()
+
+def email_taken(email):
+    return User.query.filter(User.email == email).first()
---
+++ tests/pasteapp_tests.py
@@ -0,0 +1,279 @@
+from os.path import abspath
+import unittest
+from pasteapp import create_app
+from pasteapp.database import db_session, init_db, clear_db, User
+
+class TestCase(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        self.app = create_app(abspath('config_testing.py'))
+        super(TestCase, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        self.client = self.app.test_client()
+        self.ctx = self.app.test_request_context()
+        self.ctx.push()
+        init_db()
+        user = User('test_user', 'test_user@example.com', 'password')
+        db_session.add(user)
+        db_session.commit()
+
+    def tearDown(self):
+        clear_db()
+        self.ctx.pop()
+
+class HomePageTest(TestCase):
+
+    def test_home_page_rendering(self):
+        rv = self.client.get('/')
+        assert 'Simple Code Sharing' in rv.data
+
+class AboutPageTestCase(TestCase):
+
+    def test_about_page_rendering(self):
+        rv = self.client.get('/about')
+        assert 'About' in rv.data
+
+class RegisterPageTestCase(TestCase):
+
+    def register(self, username, email, password,
+                 confirm, redirect=True):
+        """
+        Helper function to make a post request to the register method.
+        Saves on duplication in the tests.
+        """
+        return self.client.post('/register', data={
+            'username': username,
+            'email': email,
+            'password': password,
+            'confirmPassword': confirm
+        }, follow_redirects=redirect)
+
+    def test_register_page_rendering(self):
+        rv = self.client.get('/register')
+        assert 'Register Now' in rv.data
+
+    def test_login_message_if_account_holder(self):
+        """
+        This test checks that the user can go straight to the login page if
+        they already have an account.
+        """
+
+        msg = 'Already have an account? Log in now.'
+        rv = self.client.get('/register')
+        assert msg in rv.data
+
+    def test_username_field(self):
+        """
+        This test checks that the registration form has a username field.
+        """
+
+        rv = self.client.get('/register')
+        assert 'Username' in rv.data
+
+    def test_email_address_field(self):
+        """
+        This test checks that the registration form has an email address
+        field.
+        """
+
+        rv = self.client.get('/register')
+        assert 'Email Address' in rv.data
+
+    def test_password_field(self):
+        """
+        This test checks that the registration form has a password field.
+        """
+
+        rv = self.client.get('/register')
+        assert 'Password' in rv.data
+
+    def test_confirm_password_field(self):
+        """
+        This test checks that the registration form has a field to confirm
+        the password that the user entered.
+        """
+
+        rv = self.client.get('/register')
+        assert 'Confirm Password' in rv.data
+
+    def test_valid_user_registration(self):
+        """
+        This test checks that a user is successfully registered if they provide \
+        valid user information during registration.
+        """
+        msg = 'You have successfully registered and can now log in.'
+        # Checking the boundary maximum length here for the username
+        rv = self.register('a'*100, 'mister_test@example.com',
+                           'somesecurepassword', 'somesecurepassword')
+        assert msg in rv.data
+
+    def test_too_short_username_registration(self):
+        """
+        This test checks that the correct error message is displayed if the
+        user attempts to register using a username that is too short.
+        """
+        errorMsg = 'The username must be between 3 and 100 chars long.'
+        rv = self.register('bb', 'mister_test@example.com',
+                           'password', 'password')
+        assert errorMsg in rv.data
+
+    def test_too_long_username_registration(self):
+        """
+        This test checks that the correct error message is displayed if the
+        user attempts to register using a username that is too long.
+        """
+        errorMsg = 'The username must be between 3 and 100 chars long.'
+        rv = self.register('a'*101, 'mister_test@example.com',
+                           'password', 'password')
+        assert errorMsg in rv.data
+
+    def test_username_required_registration(self):
+        """
+        This test checks that an error message is displayed if the user
+        attempts to register without providing a username.
+        """
+        errorMsg = 'The username field is required.'
+        rv = self.register('', 'mister_test@example.com', 'password',
+                           'password')
+        assert errorMsg in rv.data
+
+    def test_email_required_registration(self):
+        """
+        This test checks that an error message is displayed if the user tries
+        to register without providing an email address.
+        """
+        errorMsg = 'The email address field is required.'
+        rv = self.register('mister_test', '', 'password', 'password')
+        assert errorMsg in rv.data
+
+    def test_email_too_short_registration(self):
+        """
+        This test checks that an error message is displayed if the user tries
+        to register with an email address that is too short.
+        """
+        errorMsg = 'The email address must be between 4 and 100 chars long.'
+        rv = self.register('mister_test', 'a@b', 'password', 'password')
+        assert errorMsg in rv.data
+
+    def test_email_too_long_registration(self):
+        """
+        This test checks that an error message is displayed if the user tries
+        to register with an email address that is too long.
+        """
+        errorMsg = 'The email address must be between 4 and 100 chars long.'
+        rv = self.register('mister_test', 'a'*101, 'password', 'password')
+        assert errorMsg in rv.data
+
+    def test_password_required_registration(self):
+        """
+        This test checks that an error message is displayed if the user tries
+        to register without providing a password.
+        """
+        errorMsg = 'The password field is required.'
+        rv = self.register('mister_test', 'mister_test@example.com',
+                           '', 'password')
+        assert errorMsg in rv.data
+
+    def test_passwords_must_match_registration(self):
+        """
+        This test checks that the correct error message is displayed if the
+        two passwords do not match.
+        """
+        errorMsg = 'The two passwords must match.'
+        rv = self.register('mister_test', 'mister_test@example.com',
+                           'password1', 'password2')
+        assert errorMsg in rv.data
+
+    def test_password_length_registration(self):
+        """
+        This test checks that the correct error message is displayed if the
+        user tries to register using a short password.
+        """
+        errorMsg = 'The password must be at least 8 characters long.'
+        rv = self.register('mister_test', 'mister_test@example.com',
+                           '1234567', '1234567')
+        assert errorMsg in rv.data
+
+    def test_duplicate_username_registration(self):
+        """
+        This test checks that the correct error message is displayed if the
+        user tries to register using a username that is already in use.
+        """
+        errorMsg = 'Username has already been taken.'
+        rv = self.register('test_user', 'user@example.com', 'password',
+                           'password')
+        assert errorMsg in rv.data
+
+    def test_duplicate_email_registration(self):
+        """
+        This test checks that the correct error message is displayed if the
+        user tries to register using a username that is already in use.
+        """
+        errorMsg = 'Email address has already been used.'
+        rv = self.register('mister_test', 'test_user@example.com', 'password',
+                           'password')
+        assert errorMsg in rv.data
+
+class LoginPageTestCase(TestCase):
+
+    def login(self, username, password, redirect=True):
+        """
+        Helper method to make a login post request.
+        """
+        return self.client.post('/login', data={
+            'username': username,
+            'password': password
+        }, follow_redirects=redirect)
+
+    def logout(self, redirect=True):
+        return self.client.get('/logout', follow_redirects=redirect)
+
+    def test_login_valid_credentials(self):
+        msg = 'You have logged in successfully.'
+        rv = self.login('test_user', 'password')
+        assert msg in rv.data
+
+    def test_login_wrong_username(self):
+        msg = 'Invalid username or password'
+        rv = self.login('test', 'password')
+        assert msg in rv.data
+
+    def test_login_wrong_password(self):
+        msg = 'Invalid username or password'
+        rv = self.login('test_user', 'wrong')
+        assert msg in rv.data
+
+    def test_logout_not_logged_in(self):
+        msg = 'Logged out successfully'
+        rv = self.logout()
+        assert msg not in rv.data
+
+    def test_logout_logged_in(self):
+        msg = 'Logged out successfully'
+        self.login('test_user', 'password')
+        rv = self.logout()
+        assert msg in rv.data
+
+    def test_dashboard_only_when_logged_in(self):
+        rv = self.client.get('/dashboard', follow_redirects=True)
+        assert 'You must log in first' in rv.data
+
+    def test_dashboard_logged_in(self):
+        self.login('test_user', 'password')
+        rv = self.client.get('/dashboard', follow_redirects=True)
+        assert 'Your Overview' in rv.data
+
+class SnippetTestCase(TestCase):
+
+    def create_snippet(self):
+        """
+        Helper function.
+        """
+        pass
+
+    def test_create_snippet_redirect(self):
+        pass
+
+    def test_create_snippet_no_content(self):
+        pass
