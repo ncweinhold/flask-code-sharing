dc5f71c059d79f39050548714116ec6adb20b45f
Nick Weinhold <n.c.weinhold@googlemail.com>
2012-12-11 12:32:58 +0000

    Adding form models.
    Adding in models for the Registration and Login forms.

---
+++ pasteapp/forms.py
@@ -0,0 +1,29 @@
+from flask.ext.wtf import (
+    Form, TextField, PasswordField, SubmitField, Required, Length,
+    ValidationError, EqualTo
+)
+
+class RegistrationForm(Form):
+
+    username = TextField('Username', validators = [
+        Required(message='The username field is required.'),
+        Length(min=3, max=100,
+               message='The username must be between 3 and 100 chars long.')])
+    email = TextField('Email Address', validators = [
+        Required(message='The email address field is required.'),
+        Length(min=4, max=100,
+               message='The email address must be between 4 and 100 chars long.')])
+    password = PasswordField('Password', validators = [
+        Required(message='The password field is required.'),
+        Length(min=8, message='The password must be at least 8 characters long.'),
+        EqualTo('confirmPassword', message='The two passwords must match.')])
+    confirmPassword = PasswordField('Confirm Password')
+    submit = SubmitField('Register')
+
+class LoginForm(Form):
+
+    username = TextField('Username', validators = [
+        Required(message='You must enter a username.')])
+    password = PasswordField('Password', validators = [
+        Required(message='You must enter a password.')])
+    submit = SubmitField('Login')
